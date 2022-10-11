5341c870f6ff7f074dab767a6f529217d92545d0
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-10-11 21:06:07 +0200

    Section 01: Getting Started

--- Section 01 - Getting Started/my-first-app/src/app/app.component.ts
+++ Section 01 - Getting Started/my-first-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //* HTML tag we are selection
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+
 }
