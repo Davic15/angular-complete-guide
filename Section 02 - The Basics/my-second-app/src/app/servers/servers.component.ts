6413884c3b930a798643cacb4cabda7f8f0e4c69
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-10-13 23:34:55 +0200

    Section 02: The Basics

--- Section 02 - The Basics/my-second-app/src/app/servers/servers.component.ts
+++ Section 02 - The Basics/my-second-app/src/app/servers/servers.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+//* we can use template instead of templateUrl.
+//* template can be used for some small code line (maybe three).
+//* selector: [app-server] in this case this is an HTML attribute.
+//* selector: .app-server select by class.
+//* For components we use element style selector.
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
